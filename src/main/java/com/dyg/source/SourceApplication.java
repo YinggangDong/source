2c9c162b73e66192a75ea4f6c3630ed373dd89ed
YinggangDong <believerD@aliyun.com>
2020-09-17 14:12:53 +0800

    feat:引入mysql,新增log4j2配置文件路径和数据库连接

--- src/main/java/com/dyg/source/SourceApplication.java
+++ src/main/java/com/dyg/source/SourceApplication.java
@@ -1,12 +1,15 @@
 package com.dyg.source;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SourceApplication {
 
     public static void main(String[] args) {
+        log.info("hello spring");
         SpringApplication.run(SourceApplication.class, args);
     }
 
