14b9b6092a64cf584e765d6d542a9ba073ac79cf
YinggangDong <believerD@aliyun.com>
2020-09-17 17:19:20 +0800

    feat:新增测试类

--- src/test/com/dyg/source/collection/list/ArrayListTest.java
+++ src/test/com/dyg/source/collection/list/ArrayListTest.java
@@ -1,11 +1,26 @@
 package com.dyg.source.collection.list;
 
+import com.dyg.source.SourceApplication;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 
+import static org.junit.Assert.*;
+
+/**
+ * ArrayListTest 类是 ArrayList测试类
+ *
+ * @author dongyinggang
+ * @date 2020/9/17 17:16
+ */
 @Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SourceApplication.class)
 public class ArrayListTest {
 
     @Test
@@ -17,13 +32,6 @@ public class ArrayListTest {
         log.info("add新增1个元素后：size:" + list.size()+",length:"+list.elementData.length);
         list.trimToSize();
         log.info("调用trimToSize()后的：size:" + list.size()+",length:"+list.elementData.length);
-    }
-
-    @Test
-    public void ensureCapacity() {
-    }
-
-    @Test
-    public void size() {
+        Assert.assertEquals(list.size(),list.elementData.length);
     }
 }
